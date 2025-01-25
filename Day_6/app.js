d9ab350260c333002a7a851282ae4fd3a1135a23
vikrant-gtihub <vikrantpandey267@gmail.com>
2025-01-25 12:16:49 +0530

    backend server created with crud operations

--- Day_6/app.js
+++ Day_6/app.js
@@ -31,29 +31,53 @@ app.get("/read/:filename", function (req, res) {
     }
   });
 });
+
+app.patch("/update/:filename", function (req, res) {
+  const { filename } = req.params;
+  const { filedata } = req.body;
+  const filepath = "./uploads/" + filename;
+  fs.writeFile(filepath, filedata, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send("file updated successfully");
+    }
+  });
+});
+
+app.delete("/delete/:fileName", function (req, res) {
+  const filePath = "./uploads/" + req.params.fileName;
+  fs.unlink(filePath, function (err) {
+    if (err) {
+      console.log("file can not be deleted");
+    } else {
+      res.send("file deleted successfully");
+    }
+  });
+});
+
+app.get("/get-all", function (req, res) {
+  fs.readdir("./uploads", function (err, files) {
+    if (err) {
+      console.log(err);
+    }
+
+    res.send(files);
+  });
+});
 app.listen(3000);
-
-app.patch('/update/:filename',function(req,res){
-  const {filename} = req.params;
-  const {filedata} = req.body
-  const filepath = './uploads/' + filename
-  fs.writeFile(filepath,filedata,function(err){
-    if(err){
-      console.log(err)
-    }
-    else{
-      res.send('file updated successfully')
-    }
-  })
-})
-
 /**
  * 1.fs ek moduel ahi jo ki node me install hoke milta hai
  * 2.iski help se files ko CRUD operations provide kiye jate hain.
  * 3.postman ki help se hum routes me request bhejte hain aur jarurat anusar data bhi phir alag alag api ya end points par unhe
  *  read etc karte hyain inme jis function ka styntax jaise likha hai waise hi likha hai.
- * 4.yad rahe jab routes par jakar koi naya expression create nahi kar rahe jaise ki koi naya page create karna koi naya card etc aur 
- *  sirf kuchh data retriev akr rahe hain to app.get() ka use hota hai... ja kuchh expressions create kar rahe hain yani ki kuchh modification
- *  kar rahe hain server pe kuchh data bhej rahe hain to app.post ka use hota hai aur jab hum kuchh partial update kar rahe hain server pe 
+ * 4.yad rahe jab routes par jakar koi naya expression create nahi kar rahe jaise ki koi naya page create karna koi naya card etc aur
+ *  sirf kuchh data retriev akr rahe hain to ...app.get()... ka use hota hai... ja kuchh expressions create kar rahe hain yani ki kuchh modification
+ *  kar rahe hain server pe kuchh data bhej rahe hain to ....app.post()... ka use hota hai aur jab hum kuchh partial update kar rahe hain server pe
  *  to ..app.patch() ka use hota hai...
+ * 5.backend ka koi resource delete akr rahe hain jaise databse ka koi user ya is case me file to ...app.delete()... ka use hota hai 
+ * aur sabka syntax chatgpt se kar sakte hain
+ * 6.backend par data ya to (req.query) me aata hai ya (req.body) me jab data jyada hota hai to hume (req.body) ka use karna hota hai jaise ki
+ * fiels me data aa raha ho aur jab data kam ho ...(req.query)... me bhi mil jata hai par ..req.body.. ko use akrne ke liye hume ek
+ * middleware ka use karna padta hai ...app.use(express.json())...
  */
